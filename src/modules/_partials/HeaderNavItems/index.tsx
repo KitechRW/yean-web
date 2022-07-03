9054ebdbc550bee809327dc59de399e8658e2e54
cniyindagiriye <cniyindagiriye@gmail.com>
2022-07-03 14:39:44 +0200

    add popups

--- src/modules/_partials/HeaderNavItems/index.tsx
+++ src/modules/_partials/HeaderNavItems/index.tsx
@@ -3,16 +3,28 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { headerNavs } from 'system/static';
+import WedoPopup from '../WedoPopup';
 
-const HeaderNavItems = ({
-  className = 'flex items-center',
-}) => {
+const HeaderNavItems = ({ className = 'flex items-center' }) => {
   const { pathname } = useRouter();
   return (
     <div className={className}>
       {headerNavs.map((element, index) => {
+        if (index === 1) {
+          return (
+            <WedoPopup
+              icon={<element.icon />}
+              active={pathname === element.path}
+              label={element.label}
+            />
+          );
+        }
         return (
-          <Link key={element.label} href={element.path}>
+          <Link
+            key={element.label}
+            href={element.path}
+            className="group"
+          >
             <h2
               className={`whitespace-nowrap border-r border-opacity-06 px-2 md:px-4 flex items-center font-medium space-x-2 py-4 hover:opacity-75 bg-brand-green text-white cursor-pointer ${
                 pathname === element.path ? 'bg-opacity-30' : ''
