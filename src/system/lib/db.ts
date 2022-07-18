0392e85e3409f5ce67bb18f1d256a2718630b07a
cniyindagiriye <cniyindagiriye@gmail.com>
2022-07-18 18:18:16 +0200

    add database

---
+++ src/system/lib/db.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from 'sequelize';
+
+export const sequelize = new Sequelize(
+  `${process.env.DATABASE_URL}`,
+  {
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+);
+
+export default sequelize;
