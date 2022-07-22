19280c75fff3a0571dc3c4b793cf098fe77b4b77
Ndatumuremyi paterne <60066777+ndatumuremyi@users.noreply.github.com>
2022-07-22 15:41:29 +0200

    adding partners list

---
+++ src/modules/activities/AdminPartnersActivity/data.ts
@@ -0,0 +1,15 @@
+export const partners = [
+  {
+    imageUrl: '/assets/images/partners/msc-partner.png',
+    disc: 'Noteworthy MsC microSave Consulting',
+  },
+  {
+    imageUrl: '/assets/images/partners/msc-partner.png',
+    disc: 'Noteworthy MsC microSave Consulting',
+  },
+  {
+    imageUrl: '/assets/images/partners/msc-partner.png',
+    disc: 'Noteworthy MsC microSave Consulting',
+  },
+
+];
---
+++ src/modules/activities/_Partials/Partners/AddPartner/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import { useRouter } from 'next/router';
+import CloseIcon from '@mui/icons-material/Close';
+import Team from 'modules/activities/_Partials/About/Team';
+
+const AddPartner = () => {
+  return (
+    <>
+      <div> add partner</div>
+    </>
+  );
+};
+
+export default AddPartner;
