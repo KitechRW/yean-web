1eae518aa1617f46e188f564857864c786024d5d
nkbtemmy <emmanuelnkubito2@gmail.com>
2022-08-17 10:37:41 +0200

    -ne

--- src/apis/restful/middlewares/auth.ts
+++ src/apis/restful/middlewares/auth.ts
@@ -57,24 +57,3 @@ export const userPayload = (req: {
 
   return { appThird, thirdUser, userType };
 };
-
-// export const isloggedIn = async (req:any, res: any) => {
-//   let token = req.headers.authorization;
-//   if (!token) {
-//    return false
-//   }
-
-//   try {
-//     token = token.split(' ')[1]
-//     const decoded = decode(token);
-
-//     const freshUser:any = await UserService.findByPk(decoded.id);
-
-//     if (!freshUser.status) {
-//       return false
-//     }
-//     return freshUser.toJSON()
-//   } catch (error) {
-//     return false;
-//   }
-// };
