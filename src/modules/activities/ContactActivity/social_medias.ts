6f90cb2e14dabe56d4e4a8a4163af552f008cc43
root <paternenught@gmail.com>
2022-08-26 20:12:24 +0200

    fix message width, add real links

--- src/modules/activities/ContactActivity/social_medias.ts
+++ src/modules/activities/ContactActivity/social_medias.ts
@@ -1,5 +1,32 @@
-const data = {
+import { Call, Email, Facebook,Twitter, WhatsApp, LinkedIn } from '@mui/icons-material';
+const social_medias = [
+  {
+  name:"Facebook",
+  link:"https://www.google.com",
+  color:"#4267B2",
+  logo:Facebook,
+  },
+  {
+  name:"Twitter",
+  link:"https://www.google.com",
+  color:"#1DA1F2",
+  logo:Twitter,
+  },
 
-}
+  {
+  name:"WhatsApp",
+  link:"https://www.google.com",
+  color:"#00a884",
+  logo:WhatsApp,
+  },
+  {
+  name:"LinkedIn",
+  link:"https://www.htt.com",
+  color:"#0077b5",
+  logo:LinkedIn,
+  },
 
-export default data
+
+]
+
+export default social_medias
