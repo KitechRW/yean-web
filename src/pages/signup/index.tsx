51ff6b138b584f1e7c0f82f0411c13b04c5d96fa
cniyindagiriye <cniyindagiriye@gmail.com>
2022-08-27 23:26:53 +0200

    add signup

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import type { NextPage } from 'next';
-import * as yup from 'yup';
+import joi from 'joi';
 import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { joiResolver } from '@hookform/resolvers/joi';
 import { useRouter } from 'next/router';
 import { withSessionSsr } from 'system/lib/withSession';
 import Secure from 'system/helpers/secureLs';
@@ -11,48 +11,65 @@ import isAuth from 'system/helpers/isAuth';
 import { useTranslation } from 'react-i18next';
 import Logo from 'modules/_partials/Logo';
 import Link from 'next/link';
+import { formatJoiErorr } from 'system/format';
 
-const schema = yup
+const schema = joi
   .object({
-    password: yup.string().required('Password is required'),
-    email: yup
+    password: joi.string().required(),
+    retypePassword: joi.string().required().label('Re-type Password'),
+    email: joi
       .string()
-      .required('Email is required')
-      .email('Email is invalid'),
+      .required()
+      .email({ tlds: { allow: false } }),
   })
   .required();
 
 const Signup: NextPage = () => {
   const { t } = useTranslation();
   const { push } = useRouter();
+  const [loading, setLoading] = React.useState(false);
   const [errorMessage, setErrorMessage] = React.useState(null);
+  const [successMessage, setSuccessMessage] = React.useState(null);
   const {
     register,
     handleSubmit,
+    getValues,
+    setError,
+    setValue,
+    reset,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver(schema),
+    resolver: joiResolver(schema),
   });
-  const onSubmit = (data: any) => {
-    setErrorMessage(null);
-    axios
+  const onSubmit = async (data: any) => {
+    setLoading(true);
+    await axios
       .post('/api/auth/signup', data)
       .then(response => {
-        const {
-          data: { token },
-        } = response.data;
-        console.log(token);
-        Secure.setToken(token);
-        push('/bookings');
+        const { user, message } = response.data;
+        if (user) {
+          reset();
+          setSuccessMessage(message);
+          setTimeout(() => {
+            push('/login');
+          }, 3000);
+        }
       })
       .catch(result => {
-        const { error } =
+        console.log(result);
+        const error =
           result.response?.data || result.response || result;
         console.log(error?.message);
         setErrorMessage(
           error?.message || 'Something went wrong, try again',
         );
       });
+    setLoading(false);
+    setTimeout(() => {
+      setErrorMessage(null);
+      setSuccessMessage(null);
+      setLoading(false);
+    }, 5000);
   };
   return (
     <div className="bg-top bg-cover bg-no-repeat bg-[url(/assets/images/login.svg)] flex flex-col items-center min-h-screen">
@@ -70,6 +87,13 @@ const Signup: NextPage = () => {
                 </p>
               </div>
             ) : null}
+            {successMessage ? (
+              <div className="mt-3 flex flex-col items-center rounded-lg bg-brand-green px-4 py-3">
+                <p className="text-white text-sm first-letter:uppercase">
+                  {successMessage}
+                </p>
+              </div>
+            ) : null}
             <form
               onSubmit={event => {
                 handleSubmit(onSubmit)(event);
@@ -84,9 +108,11 @@ const Signup: NextPage = () => {
                   {...register('email')}
                   className="mt-1 focus:border-primary bg-gray-202 outline-none rounded-lg border border-gray-201 px-3 py-2"
                 />
-                <p className="text-red-500 text-xs mt-1">
-                  {errors.email?.message}
-                </p>
+                {errors.email?.message && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {formatJoiErorr(`${errors.email?.message}`)}
+                  </p>
+                )}
               </label>
               <label
                 htmlFor="user-password"
@@ -101,9 +127,11 @@ const Signup: NextPage = () => {
                   {...register('password')}
                   className="mt-1 focus:border-primary bg-gray-202 outline-none rounded-lg border border-gray-201 px-3 py-2"
                 />
-                <p className="text-red-500 text-xs mt-1">
-                  {errors.password?.message}
-                </p>
+                {errors.password?.message && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {formatJoiErorr(`${errors.password?.message}`)}
+                  </p>
+                )}
               </label>
 
               <label
@@ -116,16 +144,39 @@ const Signup: NextPage = () => {
                 <input
                   id="retype-password"
                   type="password"
-                  {...register('password')}
+                  {...register('retypePassword')}
+                  onChange={event => {
+                    const value = event.target.value;
+                    if (value !== getValues('password')) {
+                      setError(
+                        'retypePassword',
+                        {
+                          type: 'focus',
+                          message: 'Password does not match',
+                        },
+                        { shouldFocus: true },
+                      );
+                    } else {
+                      setValue('retypePassword', value, {
+                        shouldDirty: true,
+                        shouldValidate: true,
+                      });
+                    }
+                  }}
                   className="mt-1 focus:border-primary bg-gray-202 outline-none rounded-lg border border-gray-201 px-3 py-2"
                 />
-                <p className="text-red-500 text-xs mt-1">
-                  {errors.password?.message}
-                </p>
+                {errors.retypePassword?.message && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {formatJoiErorr(
+                      `${errors.retypePassword?.message}`,
+                    )}
+                  </p>
+                )}
               </label>
               <button
                 type="submit"
-                className="mt-6 w-full font-semibold tracking-wide bg-brand-green px-4 py-3 rounded-lg text-white"
+                disabled={loading}
+                className="mt-6 w-full disabled:cursor-not-allowed font-semibold tracking-wide disabled:bg-brand-green/25 bg-brand-green px-4 py-3 rounded-lg text-white"
               >
                 Signup
               </button>
