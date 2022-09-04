a9dbffc2eea455fb0d0f0b15f97d9e9811915098
ndatumuremyi <paternenught@gmail.com>
2022-09-04 11:15:33 +0200

    fixing project view

--- src/pages/project/[projectId]/index.tsx
+++ src/pages/project/[projectId]/index.tsx
@@ -3,14 +3,29 @@ import Head from "next/head";
 import React from "react";
 import ProjectActivity from "modules/activities/ProjectActivity";
 import { useRouter } from 'next/router'
+import axios from "axios";
 
-const ProjectPage:NextPage = () => {
+export async function getServerSideProps({query:{projectId}}:any) {
+  return axios.get(`${process.env.DEFAULT_API}/api/projects/${projectId}`).then(resp => {
+
+    return {
+      props: { data: resp.data.data},
+    }
+  }).catch(ex => {
+
+    console.error(ex)
+    return {
+      props:{data:{}}
+    }
+  });
+}
+const ProjectPage:NextPage = (props:any) => {
   const router = useRouter();
   return <>
     <Head>
       <title>Yean</title>
     </Head>
-    <ProjectActivity id={router.query.projectId} />
+    <ProjectActivity data={props.data} />
   </>
 }
 export default ProjectPage
