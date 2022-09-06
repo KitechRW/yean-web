93b6019a55982a66c05fbb571ae6a0cbecfef1bf
ndatumuremyi <paternenught@gmail.com>
2022-09-06 22:12:27 +0200

    adding one item dashboard file

--- src/modules/activities/DashboardActivity/index.tsx
+++ src/modules/activities/DashboardActivity/index.tsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const DashboardActivity = () => {
   return (
-    <div>DashboardActivity</div>
+    <div>
+      <div className={"flex flex-col"}>
+        <div className={"flex place-content-evenly mt-5"}>
+          <div
+            className={`flex flex-col p-3 px-8 justify-center min-w-[255px] min-h-[145px] drop-shadow rounded-lg bg-white`}
+          >
+            <h2
+              className={`text-base font-semibold`}
+            >
+              Reports
+            </h2>
+            <div className="flex items-center space-x-2 mt-3">
+              <p
+                className={`bg-[#E6F3FF] p-2 px-3 rounded text-dark-green font-bold`}
+              >
+                34
+              </p>
+              <p
+                className={`text-[#AAB1B7]`}
+              >
+                Registered
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
