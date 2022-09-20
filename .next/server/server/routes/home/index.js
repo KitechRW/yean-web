c401ffe633d28a6554c900e13f2b58cdd7e68d42
cniyindagiriye <cniyindagiriye@gmail.com>
2022-09-20 13:09:56 +0200

    make it better

--- .next/server/server/routes/home/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const gamesRouter = (0, express_1.Router)();
-gamesRouter.get('/', (req, res) => {
-    res.send('Welcome to yean api');
-});
-exports.default = gamesRouter;
